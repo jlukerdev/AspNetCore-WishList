eb95a4b4da592ab82a04cf7ba6b453ecb78101fa
Josh Luker <JLuker@vulcraft-tx.com>
2018-09-12 08:54:01 -0500

    .

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      return this.View("Index");
+    }
+
+    public IActionResult Error()
+    {
+      return this.View("Error");
+    }
+  }
+}
